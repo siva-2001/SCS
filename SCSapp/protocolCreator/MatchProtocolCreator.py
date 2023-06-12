7c7556f9a15c2864a9ce57615f37848cd59dd6b6
chzhen-g <georgii2911@gmail.com>
2023-06-12 20:01:55 +0700

    update

--- SCSapp/protocolCreator/MatchProtocolCreator.py
+++ SCSapp/protocolCreator/MatchProtocolCreator.py
@@ -1,13 +1,24 @@
 from fpdf import FPDF
+import os
+
 
 class PDFProtocolCreator():
 
     def volleybalMatchProtocol(self, d):
 
+        # Определение пути к текущей директории
+        current_directory = os.getcwd()
+
+        # Определение пути к целевой директории относительно текущей директории
+        target_directory = os.path.join(current_directory, "media", "protocols")
+
+        # Переход к целевой директории
+        os.chdir(target_directory)
+
         pdf = FPDF('P', 'mm', 'A4')
 
         pdf.add_page()
-        pdf.add_font('DejaVu', '', 'DejaVuSansCondensed.ttf', uni=True)
+        pdf.add_font('DejaVu', '', current_directory+'\SCSapp\protocolCreator\DejaVuSansCondensed.ttf', uni=True)
         pdf.set_font('DejaVu', size=10)
 
         pdf.cell(0, 8, align='C', txt="ПРОТОКОЛ №", border=False)
@@ -308,13 +319,16 @@ class PDFProtocolCreator():
         #         pdf.cell(3, 8, txt='', border=False)
         #     pdf.ln(8)
 
-        pdf.output('test.pdf')
+        pdf.output('matchProtocol.pdf')
+        # Возврат к исходной рабочей директории
+        os.chdir(current_directory)
 
     def test(self):
         d = {
             'nameCompetition': 'Спортакиада',
             'place': 'ТУСУР',
             'time': '17:15',
+
             'date': '22.06.2023',
             'firstJudjeFIO': 'Павел Александрович Иванов',
             'firstCommand': {
